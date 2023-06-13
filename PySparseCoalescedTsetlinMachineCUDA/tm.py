c14e4f28387d061910b3a168ed895c5b497ac3e0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-06-13 09:51:16 +0200

    Multi patch processing

--- PySparseCoalescedTsetlinMachineCUDA/tm.py
+++ PySparseCoalescedTsetlinMachineCUDA/tm.py
@@ -356,7 +356,21 @@ class CommonTsetlinMachine():
 		for e in range(X.shape[0]):
 			cuda.memcpy_htod(self.class_sum_gpu, class_sum[e,:])
 
-			self.encode.prepared_call(self.grid, self.block, self.X_test_indptr_gpu, self.X_test_indices_gpu, self.encoded_X_gpu, np.int32(e), np.int32(self.dim[0]), np.int32(self.dim[1]), np.int32(self.dim[2]), np.int32(self.patch_dim[0]), np.int32(self.patch_dim[1]), np.int32(self.append_negated), np.int32(0))
+			self.encode_score.prepared_call(
+				self.grid,
+				self.block,
+				self.X_test_indptr_gpu,
+				self.X_test_indices_gpu,
+				self.encoded_X_gpu,
+				np.int32(e),
+				np.int32(self.dim[0]),
+				np.int32(self.dim[1]),
+				np.int32(self.dim[2]),
+				np.int32(self.patch_dim[0]),
+				np.int32(self.patch_dim[1]),
+				np.int32(self.append_negated),
+				np.int32(0)
+			)
 			cuda.Context.synchronize()
 
 			self.evaluate.prepared_call(
